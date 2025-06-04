6bf4c9caff306b7a007b3f1c2c7eefdfcde268a8
Samprina Zekio <savvinazekiou@gmail.com>
2025-06-04 12:06:13 +0300

    length.js
    Using js to calculate the number of characters

---
+++ length.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>String Test</title>
+</head>
+<body>
+
+<h1>String Test</h1>
+<script type="text/javascript">
+document.write("This is a test. " );
+test= "This is a test.";
+document.write("The Length is: " + " " + test.length ) ; 
+</script>
+
+</body>
+</html>
